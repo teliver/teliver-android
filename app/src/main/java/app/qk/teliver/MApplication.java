daf798709404f8a9b77aafc6e03512eece1f0876
Sivakumar <sivakumar.b@quadkast.com>
2022-02-09 17:23:25 +0530

    Version change

--- app/src/main/java/app/qk/teliver/MApplication.java
+++ app/src/main/java/app/qk/teliver/MApplication.java
@@ -5,6 +5,7 @@ import androidx.multidex.MultiDexApplication;
 import io.teliver.sdk.core.TLog;
 import io.teliver.sdk.core.Teliver;
 
+
 public class MApplication extends MultiDexApplication {
 
     @Override
